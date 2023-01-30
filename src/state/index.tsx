1984be682f04d2dbae883b4a240cbfbad1eb6ec1
Marcos Almeida <marcanaster@gmail.com>
2023-01-30 14:55:06 -0300

    index

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -92,7 +92,7 @@ export default function AppStateProvider(props: React.PropsWithChildren<{}>) {
     contextValue = {
       ...contextValue,
       getToken: async (user_identity, room_name) => {
-        const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
+        const endpoint = '/token';// process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
 
         console.log("***endeposint 1 ***", endpoint)
 
